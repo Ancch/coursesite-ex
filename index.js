c09b6cc25bc6dcb7f730862492ef525acdf90030
Ancch <anshk7893@gmail.com>
2024-11-17 22:31:49 +0530

    done with the app route skeleton

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
+const { adminRouter } = require('./routes/admin');
 
-app.use('/user', userRouter);
-app.use('/course', courseRouter);
+app.use('v1/user', userRouter);
+app.use('v1/admin', adminRouter);
+app.use('v1/course', courseRouter);
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,43 @@
+const { Router, application } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function(req, res) {
+    res.json({
+        message: "admin signup endpoint"
+    })
+})
+
+adminRouter.post("/login", function(req, res) {
+    res.json({
+        message: "admin login endpoint"
+    })
+})
+
+adminRouter.post("/course", function(req, res) {
+    res.json({
+        message: "admin create course endpoint"
+    })
+})
+
+adminRouter.put("/course", function(req, res) {
+    res.json({
+        message: "admin change course content endpoint"
+    })
+})
+
+adminRouter.get("/course/bulk", function(req, res) {
+    res.json({
+        message: "admin get courses endpoint"
+    })
+})
+
+adminRouter.post("/delete", function(req, res) {
+    res.json({
+        message: "admin delete course endpoint"
+    })
+})
+
+
+module.exports = {
+    adminRouter: adminRouter
+}
